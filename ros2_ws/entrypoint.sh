103ff77b6fd7d9930cb1565d4b664018a6f7050b
HulajDuszaa <163999857+HulajDuszaa@users.noreply.github.com>
2025-05-27 12:53:35 +0200

    uddating repo entrypoint.sh

--- ros2_ws/entrypoint.sh
+++ ros2_ws/entrypoint.sh
@@ -6,6 +6,13 @@ set -e
 # Source ROS 2 setup
 source /opt/ros/humble/setup.bash
 
+# Clone the manipulator_ros2 repo into /ros2_ws if it doesn't exist
+if [ ! -d "/ros2_ws/.git" ]; then
+    echo "Cloning manipulator_ros2 repository into /ros2_ws..."
+    rm -rf /ros2_ws/*  # Clean up in case the directory exists but is empty
+    git clone https://github.com/HulajDuszaa/manipulator_ros2.git /ros2_ws
+fi
+
 # Build the workspace if needed
 if [ -d "/ros2_ws/src" ]; then
     if [ ! -d "/ros2_ws/install" ] || [ -z "$(ls -A /ros2_ws/install)" ]; then
